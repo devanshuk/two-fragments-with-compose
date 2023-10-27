75b4256bef340e2893801647ca22ae999dc5f198
Devanshu Kaushik <devanshu.kaushik@carsales.com>
2023-10-27 13:04:26 +0530

    first commit

---
+++ app/src/main/java/com/example/viewmodelscoping/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.viewmodelscoping
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
+import com.example.viewmodelscoping.ui.theme.ViewModelScopingTheme
+
+class MainActivity : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/viewmodelscoping/SecondFragment.kt
@@ -0,0 +1,61 @@
+package com.example.viewmodelscoping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.viewmodelscoping.ui.theme.ViewModelScopingTheme
+
+class SecondFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
+            )
+            id = R.id.compose_view_second_fragment
+
+            setContent {
+                ViewModelScopingTheme {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text("This is the Second Fragment !")
+
+                            Button(onClick = { goBack() }) {
+                                Text("Back")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun goBack() {
+        view?.findNavController()?.popBackStack()
+    }
+
+}
